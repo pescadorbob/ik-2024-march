93b5a8b32fb524938c42bed8347e4379d473fbbb
Brent Fisher <bc.fisher@yahoo.com>
2024-04-05 17:40:09 -0700

    Couple base cases for search.

--- src/main/java/com/brent/ik/trees/BST.java
+++ src/main/java/com/brent/ik/trees/BST.java
@@ -1,9 +1,11 @@
 package com.brent.ik.trees;
 
+import com.sun.source.tree.Tree;
+
 import java.util.Objects;
 
 public class BST {
-    private final TreeNode root;
+    private TreeNode root;
 
 
     @Override
@@ -24,16 +26,47 @@ public class BST {
     }
 
     public BST() {
-        root = new TreeNode();
+
     }
 
-    public void add(int val) {
-        root.setValue(val);
+    public BST add(int val) {
+
+        if(root == null){
+            root = new TreeNode(val);
+
+        } else {
+
+        }
+        return this;
+    }
+
+    public TreeNode getRoot() {
+        return root;
+    }
+
+    public TreeNode search(int val) {
+        var curr = root;
+        while(curr != null){
+           if(val == curr.value){
+               return curr;
+           } else if (val < curr.value){
+               curr = curr.left;
+           } else {
+               curr = curr.right;
+           }
+        }
+        return null;
     }
 
     public static class TreeNode {
 
         private int value;
+        private TreeNode right;
+        private TreeNode left;
+
+        public TreeNode(int value) {
+            this.value = value;
+        }
 
         public void setValue(int val) {
             this.value = val;
@@ -44,29 +77,67 @@ public class BST {
             if (this == o) return true;
             if (o == null || getClass() != o.getClass()) return false;
             TreeNode treeNode = (TreeNode) o;
-            return value == treeNode.value;
+            if (value != treeNode.value) return false;
+            return Objects.equals(((TreeNode) o).right,this.right);
         }
 
         @Override
         public int hashCode() {
             return Objects.hash(value);
         }
+
+        public TreeNode setRight(TreeNode rightNode) {
+            this.right = rightNode;
+            return this;
+        }
+
+        @Override
+        public String toString() {
+            return "TreeNode{" +
+                    "value=" + value +
+                    ", right=" + right +
+                    '}';
+        }
+
+        public TreeNode addRight(int val) {
+            var node = new TreeNode(val);
+            this.setRight(node);
+            return node;
+        }
+
+        public TreeNode addLeft(int val) {
+            var node = new TreeNode(val);
+            this.setLeft(node);
+            return node;
+        }
+
+        private void setLeft(TreeNode node) {
+            this.left = node;
+        }
     }
     public static class Builder {
         TreeNode root;
+        TreeNode curr;
         public Builder(){
-            root = new TreeNode();
         }
         public Builder withRootNode(int val) {
+            root = new TreeNode(val);
             root.setValue(val);
+            curr = root;
             return this;
         }
 
         public BST build() {
             return new BST(this);
         }
+
+        public Builder withRightNode(int val) {
+            var rightNode = new TreeNode(val);
+            curr.setRight(rightNode);
+            return this;
+        }
     }
     public String toString(){
-        return String.format("Root %s", root.value);
+        return String.format("Root %s", root);
     }
 }
--- src/test/java/com/brent/ik/trees/BSTTest.java
+++ src/test/java/com/brent/ik/trees/BSTTest.java
@@ -2,11 +2,14 @@ package com.brent.ik.trees;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BSTTest {
     @Test
-    void shouldAddTreeNodeAtRoot_GivenEmpty(){
+    void shouldAddTreeNodeAtRoot_GivenEmpty() {
         var val = 1;
         var expectedBST = new BST.Builder().withRootNode(val).build();
         var bst = new BST();
@@ -15,4 +18,74 @@ public class BSTTest {
 
         assertThat(bst).isEqualTo(expectedBST);
     }
+
+    Map<Integer,BST.TreeNode> nodes = new HashMap<>();
+    BST createTestTree(){
+        var bst = new BST.Builder().withRootNode(44).build();
+        var rootNode = bst.getRoot();
+        nodes.put(44,rootNode);
+        var _88 = rootNode.addRight(88);
+        nodes.put(88,_88);
+        var _17 = rootNode.addLeft(17);
+        nodes.put(17,_17);
+        var _8 = _17.addLeft(8);
+        nodes.put(8,_8);
+        var _32 = _17.addRight(32);
+        nodes.put(32,_32);
+        var _28 = _32.addLeft(28);
+        nodes.put(28,_28);
+        var _29 = _28.addRight(29);
+        nodes.put(29,_29);
+        var _97 = _88.addRight(97);
+        nodes.put(97,_97);
+        var _65 = _88.addLeft(65);
+        nodes.put(65,_65);
+        var _54 = _65.addLeft(54);
+        nodes.put(54,_54);
+        var _82 = _65.addRight(82);
+        nodes.put(82,_82);
+        var _76 = _82.addLeft(76);
+        nodes.put(76,_76);
+        var _68 = _76.addLeft(68);
+        nodes.put(68,_68);
+        var _80 = _76.addRight(80);
+        nodes.put(80,_80);
+        return bst;
+    }
+
+    @Test
+    void shouldNotFindNode_givenNotPresentValue(){
+        var bst = createTestTree();
+
+        var node = bst.search(90);
+
+        assertThat(node).isNull();
+
+    }
+    @Test
+    void shouldFindNode_given13Value(){
+
+        var bst = createTestTree();
+
+        var expectedNode = bst(65);
+        var node = bst.search(65);
+
+        assertThat(node).isEqualTo(expectedNode);
+
+    }
+
+    private BST.TreeNode bst(int key) {
+        return nodes.get(key);
+    }
+//    @Test
+//    void shouldAddTreeNodeAtRight_GivenRoot() {
+//        var val = 1;
+//        var expectedBST = new BST.Builder().withRootNode(val)
+//                .withRightNode(1).build();
+//        var bst = new BST();
+//
+//        bst.add(val).add(2);
+//
+//        assertThat(bst).isEqualTo(expectedBST);
+//    }
 }
