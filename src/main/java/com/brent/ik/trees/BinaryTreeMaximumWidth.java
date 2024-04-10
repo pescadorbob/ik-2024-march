907d7fce32658a228eba4151e89573f38b48146b
Brent Fisher <bc.fisher@yahoo.com>
2024-04-09 17:43:56 -0700

    Added zero based listing

--- src/main/java/com/brent/ik/trees/BinaryTreeMaximumWidth.java
+++ src/main/java/com/brent/ik/trees/BinaryTreeMaximumWidth.java
@@ -40,6 +40,35 @@ public class BinaryTreeMaximumWidth {
         }
         return maximum_width;
     }
+	static int find_maximum_width_zero_based(TreeNode root) {
+        int maximum_width = 1;
+        Queue<Map.Entry<TreeNode, Integer>> q = new LinkedList<>();
+        q.add(new AbstractMap.SimpleEntry<>(root, 0));
+
+        while (!q.isEmpty()) {
+            int nodesInCurrentLevel = q.size();
+            int startPosition = q.peek().getValue();
+            int leftmostIndex = 0, rightmostIndex = 0;
+
+            for (int i = 0; i < nodesInCurrentLevel; i++) {
+                Map.Entry<TreeNode, Integer> currentNode = q.poll();
+                int nodePosition = currentNode.getValue() - startPosition;
+				System.out.printf("Node:%d pos:%d startPosition:%d nodePosition:%d%n",
+				     currentNode.getKey().key,currentNode.getValue(), startPosition,nodePosition);
+                leftmostIndex = Math.min(leftmostIndex, nodePosition);
+                rightmostIndex = Math.max(rightmostIndex, nodePosition);
+
+                if (currentNode.getKey().left != null) {
+                    q.add(new AbstractMap.SimpleEntry<>(currentNode.getKey().left, 2 * nodePosition ));
+                }
+                if (currentNode.getKey().right != null) {
+                    q.add(new AbstractMap.SimpleEntry<>(currentNode.getKey().right, 2 * nodePosition + 1));
+                }
+            }
+            maximum_width = Math.max(maximum_width, rightmostIndex - leftmostIndex +1);
+        }
+        return maximum_width;
+    }
 
 
     
--- src/test/java/com/brent/ik/trees/BinaryTreeMaximumWidthTest.java
+++ src/test/java/com/brent/ik/trees/BinaryTreeMaximumWidthTest.java
@@ -24,6 +24,15 @@ public class BinaryTreeMaximumWidthTest {
 		var actual = find_maximum_width(root);
 		assertThat(actual).isEqualTo(expectedWidth);
 	}		
+	@Test
+	void shouldCalculateMaxWidth_zero_based(){
+		
+		var expectedWidth = 32;
+		var root = createTestTree();
+		
+		var actual = find_maximum_width_zero_based(root);
+		assertThat(actual).isEqualTo(expectedWidth);
+	}		
 
     TreeNode createTestTree(){
         var root = new TreeNode(11);
