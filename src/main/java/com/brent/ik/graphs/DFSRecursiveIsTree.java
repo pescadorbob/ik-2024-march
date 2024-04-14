4cd62d147765c7464b37bc85c0fcb402981ae8f9
Brent Fisher <bc.fisher@yahoo.com>
2024-04-14 07:36:08 -0700

    Recursive DFS

---
+++ src/main/java/com/brent/ik/graphs/DFSRecursiveIsTree.java
@@ -0,0 +1,54 @@
+package com.brent.ik.graphs;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.LinkedList;
+import java.util.*;
+
+public class DFSRecursiveIsTree {
+
+
+    static void dfsTraversalHelper(int startNode, List<List<Integer>> graph, 
+	    List<Integer> answer, int[] isVisited,int[]components) {
+			answer.add(startNode);
+        isVisited[startNode] = components[0];
+		var adjNodes = graph.get(startNode);
+		for(Integer adj:adjNodes){
+			if(isVisited[adj]==-1){
+				dfsTraversalHelper(adj,graph,answer,isVisited,components);
+			}
+		}
+    }
+
+    public static Integer isTree(int n, List<List<Integer>> edges) {
+        List<List<Integer>> graph = new ArrayList<>();
+        List<Integer> answer = new ArrayList<>();
+        int[] isVisited = new int[n];
+
+        // Initialize graph
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
+			isVisited[i] = -1;
+        }
+
+        // Making a graph from the input edges
+        for (List<Integer> edge : edges) {
+            int u = edge.get(0);
+            int v = edge.get(1);
+            graph.get(u).add(v);
+            graph.get(v).add(u); // For undirected graph
+        }
+
+		int [] components = new int[1];
+		components[0]=0;
+        for (int i = 0; i < n; i++) {
+            if (isVisited[i]==-1) {
+				components[0] = components[0]+1;
+                dfsTraversalHelper(i, graph, answer, isVisited, components);
+            }
+        }
+
+        return components[0];
+    }
+}
---
+++ src/test/java/com/brent/ik/graphs/DFSRecursiveIsTreeTest.java
@@ -0,0 +1,60 @@
+package com.brent.ik.graphs;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static com.brent.ik.graphs.DFSRecursiveIsTree.isTree;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+class DFSRecursiveIsTreeTest {
+
+	public static class GraphData {
+		private int n;
+		private List<List<Integer>> edges;
+
+		public int getN() {
+			return n;
+		}
+
+		public void setN(int n) {
+			this.n = n;
+		}
+
+		public List<List<Integer>> getEdges() {
+			return edges;
+		}
+
+		public void setEdges(List<List<Integer>> edges) {
+			this.edges = edges;
+		}
+	}
+
+	
+	@Test
+	void shouldFindNumComponents_givenGraph() throws Exception {
+		var inputJson = """
+				{
+					"n": 6,
+					"edges": [
+					[0, 1],
+					[0, 2],
+					[0, 4],
+					[2, 3]
+					]
+				}
+""";
+		var json = new ObjectMapper();
+		GraphData graphData = json.readValue(inputJson,GraphData.class);
+		
+		var actual = isTree(graphData.n,graphData.edges);
+		assertThat(actual).isEqualTo(2);
+	}		
+}
+/**
+<T> T readValue(String content,
+                       com.fasterxml.jackson.core.type.TypeReference<T> valueTypeRef)
+*/
