49612a56ba4086f3061bcedfb7b2953d3e4e832f
Brent Fisher <bc.fisher@yahoo.com>
2024-09-21 11:44:26 -0600

    Code cleanup and optimize imports.

--- src/test/java/com/brent/ik/sort/RadixSortTest.java
+++ src/test/java/com/brent/ik/sort/RadixSortTest.java
@@ -1,19 +1,30 @@
 package com.brent.ik.sort;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
+import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class RadixSortTest {
 
-    @Test
-    void radix_sort() {
-        var input = new ArrayList(Arrays.asList(5, 8, 3, 9, 4, 1, 7));
-        var expectedOutput = Arrays.asList(1,3,4,5,7,8,9);
+    private static Stream<Arguments> provideArrays() {
+        return Stream.of(
+                Arguments.of(list(5,8,3,9,4,1,7), list(1,3,4,5,7,8,9))
+        );
+    }
+    private static ArrayList<Integer> list(Integer ... nums){
+        return new ArrayList<>(asList(nums));
+    }
+    @ParameterizedTest
+    @MethodSource("provideArrays")
+
+    void radix_sort(ArrayList input,ArrayList expectedOutput) {
         var actualOutput = RadixSort.radix_sort(input);
         assertThat(actualOutput).isEqualTo(expectedOutput);
     }
