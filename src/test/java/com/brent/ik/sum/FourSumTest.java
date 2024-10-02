8ef6f6cf3c494371d65e6ccb920415721908afeb
Brent Fisher <bc.fisher@yahoo.com>
2024-10-01 21:25:52 -0700

    More Tests Run

--- src/test/java/com/brent/ik/sum/FourSumTest.java
+++ src/test/java/com/brent/ik/sum/FourSumTest.java
@@ -1,6 +1,5 @@
 package com.brent.ik.sum;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -21,16 +20,25 @@ public class FourSumTest {
 
     private static Stream<Arguments> shouldFindUniqueQuadruplesThatSumUpToGivenValue() {
         return Stream.of(
-                Arguments.of(list(0,0,1,3,2,-1),3, lists(list(-1,0,1,3),list(0,0,1,2)))
+                Arguments.of(list(0, 0, 1, 3, 2, -1), 3, lists(list(-1, 0, 1, 3), list(0, 0, 1, 2))),
+                Arguments.of(list(-3, 0, 0, 1, 3, 2, -1), 3,
+                        lists(
+                                list(-1, 0, 1, 3),
+                                list(-3, 1, 2, 3),
+                                list(0, 0, 1, 2))),
+                Arguments.of(list(-3, 0, 0, 1, 6, 2, -1), 3,
+                        lists(list(0, 0, 1, 2),
+                                list(-3, 0, 0, 6),
+                                list(-3, -1, 1, 6)))
 
         );
     }
 
     @ParameterizedTest
     @MethodSource
-    void shouldFindUniqueQuadruplesThatSumUpToGivenValue(ArrayList arr,int target,ArrayList<ArrayList<Integer>> expected){
+    void shouldFindUniqueQuadruplesThatSumUpToGivenValue(ArrayList arr, int target, ArrayList<ArrayList<Integer>> expected) {
 
-        var actual = fourSum(arr,target);
+        var actual = fourSum(arr, target);
         assertThat(actual).containsOnlyElementsOf(expected);
     }
 
@@ -39,21 +47,21 @@ public class FourSumTest {
         // -1,0,0,1,2
         // try the first one [-1], then find 3 numbers that add to target minus first one
         var leftIndex = 0;
-        var rightIndex = arr.size()-1;
+        var rightIndex = arr.size() - 1;
         var results = new HashSet<ArrayList<Integer>>();
-        while(leftIndex<rightIndex-1){
+        while (leftIndex < rightIndex - 1) {
             var firstOne = arr.get(leftIndex);
             var threeSumTarget = target - firstOne;
-            var otherThree = threeSum(arr,leftIndex+1,rightIndex,threeSumTarget);
-            addThreeSumResultToFirstOne(firstOne,otherThree,results);
-            leftIndex ++;
+            var otherThree = threeSum(arr, leftIndex + 1, rightIndex, threeSumTarget);
+            addThreeSumResultToFirstOne(firstOne, otherThree, results);
+            leftIndex++;
         }
         return new ArrayList<>(results);
 
     }
 
     private void addThreeSumResultToFirstOne(Integer firstOne, ArrayList<ArrayList<Integer>> otherThree, Set<ArrayList<Integer>> results) {
-        otherThree.forEach(it->{
+        otherThree.forEach(it -> {
             var result = new ArrayList<Integer>();
             result.add(firstOne);
             result.addAll(it);
@@ -66,22 +74,29 @@ public class FourSumTest {
         Collections.sort(arr);
     }
 
-    private ArrayList<ArrayList<Integer>> threeSum(ArrayList<Integer> arr, int left, int right, int target) {
-        var firstOne = arr.get(left);
-        left ++;
-        var newTarget = target-firstOne;
+    private ArrayList<ArrayList<Integer>> threeSum(ArrayList<Integer> arr, int left, int right, int threeSumTarget) {
         var results = new ArrayList<ArrayList<Integer>>();
-        while(left<right){
+        while (left < right - 1) {
+            var firstOne = arr.get(left);
+            var twoSumTarget = threeSumTarget - firstOne;
+            twoSum(arr, left + 1, right, twoSumTarget, results, firstOne);
+            left++;
+        }
+        return results;
+    }
+
+    private static void twoSum(ArrayList<Integer> arr, int left, int right, int newTarget, ArrayList<ArrayList<Integer>> results, Integer firstOne) {
+        while (left < right) {
+
             var sum = arr.get(left) + arr.get(right);
-            if(sum == newTarget){
-                results.add(new ArrayList(Arrays.asList(firstOne,arr.get(left),arr.get(right))));
+            if (sum == newTarget) {
+                results.add(new ArrayList(Arrays.asList(firstOne, arr.get(left), arr.get(right))));
                 left++;
-            } else if(sum>newTarget){
+            } else if (sum > newTarget) {
                 right--;
             } else {
                 left++;
             }
         }
-        return results;
     }
 }
