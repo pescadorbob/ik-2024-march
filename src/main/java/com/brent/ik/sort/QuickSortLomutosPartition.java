bd61982e912ff454d15785dcccf6e776da0a589b
Brent Fisher <bc.fisher@yahoo.com>
2024-10-16 01:00:58 +0200

    3rd try

--- src/main/java/com/brent/ik/sort/QuickSortLomutosPartition.java
+++ src/main/java/com/brent/ik/sort/QuickSortLomutosPartition.java
@@ -3,43 +3,46 @@ package com.brent.ik.sort;
 import java.util.ArrayList;
 import java.util.Random;
 
+
 public class QuickSortLomutosPartition extends Sorter {
 
-    private static int pickPivot(int start, int end) {
-        var pivotPoint = new Random(System.currentTimeMillis()).nextInt(start, end);
-        return pivotPoint;
-    }
 
     public ArrayList<Integer> sort(ArrayList<Integer> arr) {
-
-
-        helper(arr, 0, arr.size() - 1);
+        helper(arr, 0, arr.size()-1);
         return arr;
     }
 
-    private void helper(ArrayList<Integer> arr, int start, int end) {
-        if (start >= end) return;
-        int pivot = pickPivot(start, end);
+    private static void helper(ArrayList<Integer> arr, int start, int end) {
+        if (start >= end) return; // leaf node
 
-        int pivotPoint = partition(arr, start, end, pivot);
+        int pivotPoint = pickPivot(start, end);
 
-        helper(arr, start, pivotPoint - 1);
-        helper(arr, pivotPoint + 1, end);
+        int partitionIndex = partition(arr, start, end, pivotPoint);
+
+        helper(arr, start, partitionIndex - 1);
+        helper(arr, partitionIndex + 1, end);
+
     }
 
-    private int partition(ArrayList<Integer> arr, int start, int end, int pivot) {
-        swap(arr,start,pivot);
+    private static int partition(ArrayList<Integer> arr, int start, int end, int pivot) {
 
         int smaller = start;
-        for(int larger = start;larger<=end;larger++){
-            var ele = arr.get(larger);
-            if(ele<arr.get(start)){
-                smaller ++;
-                swap(arr,smaller,larger);
+        swap(arr, start, pivot);
+
+        for (int larger = start; larger <= end; larger++) {
+            int ele = arr.get(larger);
+            if (ele < arr.get(start)) {
+                smaller++;
+                swap(arr, smaller, larger);
             }
         }
-        swap(arr,smaller,start);
+        swap(arr, start, smaller);
         return smaller;
+
+    }
+
+    private static int pickPivot(int start, int end) {
+        return new Random(System.currentTimeMillis()).nextInt(start, end);
     }
 
 }
