0ac71a6f5f3c640166a35a04ff1c501f3eb8f08e
Brent Fisher <bc.fisher@yahoo.com>
2024-10-16 00:51:08 +0200

    lomuto's partitioning added

--- src/main/java/com/brent/ik/sort/QuickSortLomutosPartition.java
+++ src/main/java/com/brent/ik/sort/QuickSortLomutosPartition.java
@@ -4,11 +4,6 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class QuickSortLomutosPartition extends Sorter {
-    private static ArrayList<Integer> createAuxArray(int start, int end) {
-        var aux = new ArrayList<Integer>();
-        while (aux.size() < end - start + 1) aux.add(null);
-        return aux;
-    }
 
     private static int pickPivot(int start, int end) {
         var pivotPoint = new Random(System.currentTimeMillis()).nextInt(start, end);
@@ -16,45 +11,34 @@ public class QuickSortLomutosPartition extends Sorter {
     }
 
     public ArrayList<Integer> sort(ArrayList<Integer> arr) {
+
+
         helper(arr, 0, arr.size() - 1);
         return arr;
     }
 
     private void helper(ArrayList<Integer> arr, int start, int end) {
-        // leaf worker
         if (start >= end) return;
-        // internal node worker
-        int pivotPoint = pickPivot(start, end);
+        int pivot = pickPivot(start, end);
 
-        int pivotIndex = partition(arr, start, end, pivotPoint);
+        int pivotPoint = partition(arr, start, end, pivot);
 
-        helper(arr, start, pivotIndex - 1);
-        helper(arr, pivotIndex + 1, end);
+        helper(arr, start, pivotPoint - 1);
+        helper(arr, pivotPoint + 1, end);
     }
 
-    /**
-     * @param arr
-     * @param start
-     * @param end
-     * @param pivotPoint
-     * @return pivotIndex
-     */
-    private int partition(ArrayList<Integer> arr, int start, int end, int pivotPoint) {
+    private int partition(ArrayList<Integer> arr, int start, int end, int pivot) {
+        swap(arr,start,pivot);
 
-        var smaller = start;
-
-        var pivot = arr.get(pivotPoint);
-        swap(arr, start, pivotPoint);
-
-        for (int larger=start+1; larger<=end; larger++) {
+        int smaller = start;
+        for(int larger = start;larger<=end;larger++){
             var ele = arr.get(larger);
-            if(ele < pivot){
-                smaller ++ ;
-                swap(arr,larger,smaller);
+            if(ele<arr.get(start)){
+                smaller ++;
+                swap(arr,smaller,larger);
             }
         }
-        swap(arr,start,smaller);
-
+        swap(arr,smaller,start);
         return smaller;
     }
 
