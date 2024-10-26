6b6ce4a34630769e9d0ed114a18e460fa6bca1b5
Brent Fisher <bc.fisher@yahoo.com>
2024-10-26 14:03:22 -0700

    parameterized

--- src/test/java/com/brent/ik/sort/OnlineMedianTest.java
+++ src/test/java/com/brent/ik/sort/OnlineMedianTest.java
@@ -1,22 +1,40 @@
 package com.brent.ik.sort;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.util.Arrays.array;
 import static org.assertj.core.util.Arrays.nonNullElementsIn;
+import static org.junit.jupiter.params.provider.Arguments.of;
 
 public class OnlineMedianTest {
+    
 
-    @Test
-    void shouldComputeMedianAfterEachElement() {
+    private static Stream<Arguments> shouldComputeMedianAfterEachElement() {
+        var input = list();
+        var expected = nonNullElementsIn(array(3, 3, 4, 6, 4));
+        return Stream.of(
+                of(input,expected));
+
+    }
+
+    private static ArrayList<Integer> list() {
         var input = new ArrayList<>(nonNullElementsIn(array(3, 4, 8, 12, 0)));
-        var expectedOutput = nonNullElementsIn(array(3, 3, 4, 6, 4));
+        return input;
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void shouldComputeMedianAfterEachElement(ArrayList<Integer> input, ArrayList<Integer> expected) {
         var actualOutput = online_median(input);
-        assertThat(actualOutput).isEqualTo(expectedOutput);
+        assertThat(actualOutput).isEqualTo(expected);
     }
 
     static ArrayList<Integer> online_median(ArrayList<Integer> stream) {
