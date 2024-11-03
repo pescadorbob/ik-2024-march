e8c78982790274ca8e217c116683a579b65b589c
Brent Fisher <bc.fisher@yahoo.com>
2024-11-03 08:08:50 -0800

    Adding bigger input/output.

--- src/test/java/com/brent/ik/sort/SortTest.java
+++ src/test/java/com/brent/ik/sort/SortTest.java
@@ -1,11 +1,14 @@
 package com.brent.ik.sort;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -27,8 +30,44 @@ public abstract class SortTest {
                         list(0, 1, 2, 3, 4, 5, 6, 9)),
                 Arguments.of(list(-1, 0, 5, 6, 3, 9, 2, 4, 1, 0),
                         list(-1, 0, 0, 1, 2, 3, 4, 5, 6, 9))
-//                ,Arguments.of(load("input013.txt"))
+                ,Arguments.of(load("input017.json"),
+                        loadOutput("output017.json"))
         );
+    }
+    private static ArrayList<Integer> load(String fileName)  {
+        var stream = Thread.currentThread().getContextClassLoader().getResourceAsStream(fileName);
+        try {
+            var ints = new ObjectMapper().readValue(stream,TestInput.class).arr;
+            var response = new ArrayList<Integer>();
+            Collections.addAll(response,ints);
+            return response;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    private static ArrayList<Integer> loadOutput(String fileName)  {
+        var stream = Thread.currentThread().getContextClassLoader().getResourceAsStream(fileName);
+        try {
+            var ints = new ObjectMapper().readValue(stream,Integer[].class);
+            var response = new ArrayList<Integer>();
+            Collections.addAll(response,ints);
+            return response;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    public static class TestInput {
+        Integer[] arr;
+
+        public Integer[] getArr() {
+            return arr;
+        }
+
+        public void setArr(Integer[] arr) {
+            this.arr = arr;
+        }
     }
 
     private static ArrayList<Integer> list(Integer... array) {
@@ -37,7 +76,7 @@ public abstract class SortTest {
 
     @ParameterizedTest
     @MethodSource("provideArrays")
-    public void testSelectionSortSortArrayOfTenItems(ArrayList<Integer> items, ArrayList<Integer> expectedItems) {
+    public void testSelectionSortSortArrayOfItems(ArrayList<Integer> items, ArrayList<Integer> expectedItems) {
 
         var actual = getSorter().sort(items);
         assertThat(actual).isEqualTo(expectedItems);
