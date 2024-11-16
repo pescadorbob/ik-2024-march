c4d3fc97ecb6fbaa1510d3e56229dfe6e15e9c74
Brent Fisher <bc.fisher@yahoo.com>
2024-11-16 10:36:21 -0700

    updated packaging to keep it all in one file.

---
+++ src/test/java/com/brent/ik/sort/QuickSortPartitionTest.java
@@ -0,0 +1,41 @@
+package com.brent.ik.sort;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import static java.util.Collections.swap;
+
+
+public class QuickSortPartitionTest extends SortTest implements Sorter {
+
+
+    public ArrayList<Integer> sort(ArrayList<Integer> arr) {
+        helper(arr,0,arr.size()-1);
+        return arr;
+    }
+    private void helper(ArrayList<Integer> arr, int start, int end){
+        if(start>=end) return;
+        int pivot = partition(arr,start,end);
+        helper(arr,start,pivot-1);
+        helper(arr,pivot+1,end);
+    }
+    private int partition(ArrayList<Integer> arr,int start,int end){
+        int smaller = start;
+        int pivot = new Random(System.currentTimeMillis()).nextInt(start,end);
+        swap(arr,pivot,start);
+        for(int larger = start;larger<=end;larger++){
+            if(arr.get(larger)<arr.get(start)){
+                smaller++;
+                swap(arr,smaller,larger);
+            }
+        }
+        swap(arr,start,smaller);
+        return smaller;
+    }
+
+
+    @Override
+    Sorter getSorter() {
+        return this;
+    }
+}
