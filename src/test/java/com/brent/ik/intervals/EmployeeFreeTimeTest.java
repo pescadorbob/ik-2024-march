ccedacdc8e800b10f18c598367fbcde8d30e835f
Brent Fisher <bc.fisher@yahoo.com>
2024-12-01 12:32:56 -0700

    Refactored for readability

--- src/test/java/com/brent/ik/intervals/EmployeeFreeTimeTest.java
+++ src/test/java/com/brent/ik/intervals/EmployeeFreeTimeTest.java
@@ -8,30 +8,34 @@ import java.util.regex.MatchResult;
 
 import static java.lang.Math.max;
 import static java.lang.Math.min;
+import static java.util.Collections.addAll;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class EmployeeFreeTimeTest {
     @Test
     void shouldCreateEmployeeFreeTime_givenWorkTimeIntervals() {
         var schedules = new ArrayList<List<Interval>>();
-        var schedule = new ArrayList<Interval>();
-        schedule.add(anInterval(1, 2));
-        schedule.add(anInterval(5, 6));
-        schedules.add(schedule);
+        schedule(schedules, anInterval(1, 2), anInterval(5, 6));
+        schedule(schedules, anInterval(1, 3));
+        schedule(schedules, anInterval(4,10));
 
-        var schedule2 = new ArrayList<Interval>();
-        schedule2.add(anInterval(1, 3));
-        schedules.add(schedule2);
 
-        var schedule3 = new ArrayList<Interval>();
-        schedule3.add(new Interval(4, 10));
-        schedules.add(schedule3);
-
-        var expected = new ArrayList<Interval>();
-        expected.add(anInterval(3, 4));
+        var expected = schedule(anInterval(3,4));
         var actual = freeTime(schedules);
         assertThat(actual).usingRecursiveFieldByFieldElementComparator().isEqualTo(expected);
 
+    }
+
+    private static ArrayList<Interval> schedule(Interval ... intervals) {
+        var expected = new ArrayList<Interval>();
+        addAll(expected,intervals);
+        return expected;
+    }
+
+    private static void schedule(ArrayList<List<Interval>> schedules, Interval... intervals) {
+        var schedule = new ArrayList<Interval>();
+        addAll(schedule, intervals);
+        schedules.add(schedule);
     }
 
     static List<Interval> freeTime(List<List<Interval>> schedules) {
@@ -42,45 +46,47 @@ public class EmployeeFreeTimeTest {
     static class EmployeeFreeTime {
 
 
-        Map<List<Interval>,Integer> indexes = new HashMap<>();
+        Map<List<Interval>, Integer> indexes = new HashMap<>();
 
         private List<Interval> freeTimeImpl(List<List<Interval>> schedules) {
-            var sToIMap = new HashMap<Interval,List<Interval>>();
-            var minHeap = new PriorityQueue<Interval>((a,b)->a.start-b.end);
-            for(List<Interval> schedule : schedules){
+            var sToIMap = new HashMap<Interval, List<Interval>>();
+            var minHeap = new PriorityQueue<Interval>((a, b) -> a.start - b.end);
+            for (List<Interval> schedule : schedules) {
                 var firstElement = removeFirst(schedule);
-                indexes.put(schedule,1);
+                indexes.put(schedule, 1);
                 minHeap.add(firstElement);
-                sToIMap.put(firstElement,schedule);
+                sToIMap.put(firstElement, schedule);
             }
             var result = new ArrayList<Interval>();
-            while(!minHeap.isEmpty()){
+            while (!minHeap.isEmpty()) {
                 var interval = minHeap.remove();
                 var schedule = sToIMap.get(interval);
-                merge(result,interval);
+                merge(result, interval);
                 var firstElement = removeFirst(schedule);
-                if(firstElement != null) {
+                if (firstElement != null) {
                     minHeap.add(firstElement);
-                    sToIMap.put(firstElement,schedule);
+                    sToIMap.put(firstElement, schedule);
                 }
             }
             return calculateIntervalGaps(result);
 
         }
-        private List<Interval> calculateIntervalGaps(List<Interval> intervals){
+
+        private List<Interval> calculateIntervalGaps(List<Interval> intervals) {
             var result = new ArrayList<Interval>();
-            for(int i=0;i<intervals.size()-1;i++){
-                var newInterval = anInterval(intervals.get(i).end,intervals.get(i+1).start);
+            for (int i = 0; i < intervals.size() - 1; i++) {
+                var newInterval = anInterval(intervals.get(i).end, intervals.get(i + 1).start);
                 result.add(newInterval);
             }
             return result;
         }
-        private void merge(List<Interval> existing,Interval interval){
-            if(existing.size()==0){
+
+        private void merge(List<Interval> existing, Interval interval) {
+            if (existing.size() == 0) {
                 existing.add(interval);
             }
-            var last = existing.get(existing.size()-1);
-            if(last.end < interval.start || interval.end < last.start){
+            var last = existing.get(existing.size() - 1);
+            if (last.end < interval.start || interval.end < last.start) {
                 // no overlap
                 existing.add(interval);
             } else {
@@ -95,11 +101,11 @@ public class EmployeeFreeTimeTest {
 
         private Interval removeFirst(List<Interval> schedule) {
             var currentIndex = 0;
-            if(indexes.containsKey(schedule)){
+            if (indexes.containsKey(schedule)) {
                 currentIndex = indexes.get(schedule);
 
             }
-            if(currentIndex < schedule.size()) {
+            if (currentIndex < schedule.size()) {
                 var interval = schedule.get(currentIndex);
                 indexes.put(schedule, ++currentIndex);
                 return interval;
