934790fb557ea7249feeb773c6ade4598bcd423c
Brent Fisher <bc.fisher@yahoo.com>
2024-12-05 20:21:50 -0700

    Redefined the comparison operator, rather than re-initializing the array.

--- src/test/java/com/brent/ik/kth/KClosestPointsToOriginTest.java
+++ src/test/java/com/brent/ik/kth/KClosestPointsToOriginTest.java
@@ -4,8 +4,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import javax.lang.model.type.ArrayType;
-import java.sql.Array;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -18,9 +16,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class KClosestPointsToOriginTest {
     private static Stream<Arguments> provideArrays() {
         return Stream.of(
-                Arguments.of(asList(asList(1, 3),asList(-2,2)), 1, asList(asList(-2,2))),
-                Arguments.of(asList(asList(1, 3),asList(-2,2),asList(10,10),asList(1,1)), 2, asList(asList(1,1),asList(-2,2))),
-                Arguments.of(asList(asList(3, 3),asList(5,-1),asList(-2,4)), 2, asList(asList(3,3),asList(-2,4)))
+                Arguments.of(asList(asList(1, 3), asList(-2, 2)), 1, asList(asList(-2, 2))),
+                Arguments.of(asList(asList(1, 3), asList(-2, 2), asList(10, 10), asList(1, 1)), 2, asList(asList(1, 1), asList(-2, 2))),
+                Arguments.of(asList(asList(3, 3), asList(5, -1), asList(-2, 4)), 2, asList(asList(3, 3), asList(-2, 4)))
         );
     }
 
@@ -33,46 +31,50 @@ public class KClosestPointsToOriginTest {
     }
 
     private List<List<Integer>> kClosest(List<List<Integer>> points, Integer k) {
-        helper(points,0,points.size()-1,k);
+        helper(points, 0, points.size() - 1, k);
         var results = new ArrayList<List<Integer>>();
-        for(int i=0;i<k;i++){
-            results.add(asList(points.get(i).get(0),points.get(i).get(1)));
+        for (int i = 0; i < k; i++) {
+            results.add(asList(points.get(i).get(0), points.get(i).get(1)));
         }
         return results;
     }
-    private static void helper(List<List<Integer>> points,int start, int end, int k){
-        if(start == end) return;
-        int pivot = partition(points,start,end);
-        if(pivot == k) {
+
+    private static void helper(List<List<Integer>> points, int start, int end, int k) {
+        if (start == end) return;
+        int pivot = partition(points, start, end);
+        if (pivot == k) {
             // lucky case!
             return;
         }
-        if(k < pivot){
-            helper(points,start,pivot-1,k);
+        if (k < pivot) {
+            helper(points, start, pivot - 1, k);
         } else {
-            helper(points,pivot+1,end,k);
+            helper(points, pivot + 1, end, k);
         }
     }
-    private static int partition(List<List<Integer>> points,int start,int end){
-        int pIndex = new Random(System.currentTimeMillis()).nextInt(start,end);
+
+    private static int partition(List<List<Integer>> points, int start, int end) {
+        int pIndex = new Random(System.currentTimeMillis()).nextInt(start, end);
         int smaller = start;
-        swap(points,start,pIndex);
-        int pivot = squared(points.get(start));
-        for(int larger = smaller+1;larger<=end;larger++){
-            if(squared(points.get(larger)) < pivot){
-                smaller ++;
-                swap(points,smaller,larger);
+        swap(points, start, pIndex);
+
+        for (int larger = smaller + 1; larger <= end; larger++) {
+            if (compare(points,larger,pIndex)) {
+                smaller++;
+                swap(points, smaller, larger);
             }
         }
-        swap(points,start,smaller);
+        swap(points, start, smaller);
         return smaller;
     }
-
-
-
-    private static Integer squared(List<Integer> c) {
-        return c.get(0)*c.get(0) + c.get(1)*c.get(1);
+    private static boolean compare(List<List<Integer>> points, int larger, int pivot){
+        return (points.get(larger).get(0)*points.get(larger).get(0) +
+                points.get(larger).get(1)*points.get(larger).get(1) <
+                points.get(pivot).get(0)*points.get(pivot).get(0) +
+                        points.get(pivot).get(1)*points.get(pivot).get(1));
     }
 
-
+    private static Integer squared(List<Integer> c) {
+        return c.get(0) * c.get(0) + c.get(1) * c.get(1);
+    }
 }
