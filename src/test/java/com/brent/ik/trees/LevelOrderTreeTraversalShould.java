e6f9abf0a35113afd056564625d2d5609b036b45
Brent Fisher <bc.fisher@yahoo.com>
2025-01-12 14:45:11 -0700

    Split the traversal into its rows.

--- src/test/java/com/brent/ik/trees/LevelOrderTreeTraversalShould.java
+++ src/test/java/com/brent/ik/trees/LevelOrderTreeTraversalShould.java
@@ -5,9 +5,12 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
 import static com.brent.ik.trees.TreeNodeBuilder.aNode;
+import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LevelOrderTreeTraversalShould {
@@ -16,35 +19,34 @@ public class LevelOrderTreeTraversalShould {
         return Stream.of(
                 Arguments.of(aNode('A')
                         .withLeft('B')
-                        .withRight(aNode('C').withRight('D')).build(), "ABCD"),
-                Arguments.of(aNode('A')
-                                .withLeft(aNode('B').withLeft('C').withRight('D')).build()
-                        , "ABCD")
+                        .withRight(aNode('C').withRight('D')).build(),
+                        asList(asList('A'),asList('B','C'),asList('D')))
         );
     }
 
     @ParameterizedTest
     @MethodSource("testArguments")
-    void produceTheTreeByLevel_givenATree(TreeNode<Character> tree, String expected) {
+    void produceTheTreeByLevel_givenATree(TreeNode<Character> tree, List<String> expected) {
         var actual = levelOrderTraversal(tree);
         assertThat(actual).isEqualTo(expected);
     }
 
-    private String levelOrderTraversal(TreeNode<Character> tree) {
+    private List<List<Character>> levelOrderTraversal(TreeNode<Character> tree) {
         var q = new ArrayDeque<TreeNode<Character>>();
         q.add(tree);
-        var result = new StringBuffer();
+        var solution = new ArrayList<List<Character>>();
         while (!q.isEmpty()) {
-            var node = q.poll();
-            result.append(node.value);
-            if (node.left != null) {
-                q.add(node.left);
+            var size = q.size();
+            var row = new ArrayList<Character>();
+            for(int i=0;i<size;i++){
+                var node = q.poll();
+                row.add(node.value);
+                if(node.left!=null) q.add(node.left);
+                if(node.right!=null) q.add(node.right);
             }
-            if (node.right != null) {
-                q.add(node.right);
-            }
+            solution.add(row);
         }
-        return result.toString();
+        return solution;
     }
 
 
