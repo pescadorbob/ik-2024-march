081712f359a452bfe91120585046c0284001275a
Brent Fisher <bc.fisher@yahoo.com>
2025-01-24 16:38:32 -0700

    Init Poker chip distributor

---
+++ src/test/java/com/brent/ik/substrings/LongestSubstringWithoutRepeatingCharactersShould.java
@@ -0,0 +1,50 @@
+package com.brent.ik.substrings;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.HashSet;
+import java.util.stream.Stream;
+
+import static java.lang.Math.max;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LongestSubstringWithoutRepeatingCharactersShould {
+    public static Stream<Arguments> characters() {
+        return Stream.of(
+                Arguments.of("abcabcbb",3),
+                Arguments.of("bbb",1),
+                Arguments.of(" ",1),
+                Arguments.of("dvdf",3),
+                Arguments.of("pwwkew",3)
+        );
+    }
+
+    /*
+     O(n2)
+     */
+    @ParameterizedTest
+    @MethodSource("characters")
+    void produce_the_length_of_the_longest_substring_give_characters(String characters,int expected){
+
+        var actual = lengthOfLongestSubstring(characters);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    public int lengthOfLongestSubstring(String characters) {
+        int longest = 0;
+        int l = 0;
+        var longestString = new HashSet<Character>();
+        for (int right = 0; right < characters.length(); right++) {
+            while(longestString.contains(characters.charAt(right))){
+                longestString.remove(characters.charAt(l));
+                l++;
+            }
+            longestString.add(characters.charAt(right));
+            longest = Math.max(longest,right-l+1);
+        }
+        return longest;
+    }
+}
--- src/test/java/com/brent/ik/sum/AddTwoLinkedListNumbersShould.java
+++ src/test/java/com/brent/ik/sum/AddTwoLinkedListNumbersShould.java
@@ -1,69 +1,104 @@
 package com.brent.ik.sum;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.LinkedList;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AddTwoLinkedListNumbersShould {
 
-    @Test
-    void parse_out_the_linked_list_into_numbers_then_add_and_provide_the_solution_as_a_linked_list(){
-        var l1 = new ListNode(2,new ListNode(4,new ListNode(3)));
-        var l2 = new ListNode(5,new ListNode(6,new ListNode(4)));
+    public static Stream<Arguments> inputs() {
+        return Stream.of(
+                Arguments.of(
+                        toNodes(2, 4, 3),
+                        toNodes(5, 6, 4),
+                        toNodes(7, 0, 8)
+                ),
+                Arguments.of(
+                        toNodes(9, 9, 9, 9, 9, 9, 9),
+                        toNodes(9, 9, 9, 9),
+                        toNodes(8, 9, 9, 9, 0, 0, 0, 1)
+                ),
+                Arguments.of(
+                        toNodes(9),
+                        toNodes(1, 9, 9, 9, 9, 9, 9, 9, 9, 9),
+                        toNodes(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1)
+                )
+        );
+    }
 
-        var expected = new ListNode(7,new ListNode(0,new ListNode(8)));
+    private static ListNode toNodes(int... ints) {
+        ListNode prev = null;
+        for (int i = ints.length - 1; i >= 0; i--) {
+            prev = new ListNode(ints[i], prev);
+        }
+        return prev;
+    }
 
-        var actual = addTwoNumbers(l1,l2);
+    @ParameterizedTest
+    @MethodSource("inputs")
+    void parse_out_the_linked_list_into_numbers_then_add_and_provide_the_solution_as_a_linked_list(ListNode l1, ListNode l2, ListNode expected) {
+
+        var actual = addTwoNumbers(l1, l2);
 
         assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
 
     }
+
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int l1Value=0;
-        int l2Value=0;
-        var place = 1;
-        while(l1!=null){
-            l1Value += l1.val*place;
-            l1 = l1.next;
-            place *= 10;
-        }
-        place = 1;
-        while(l2!=null){
-            l2Value += l2.val*place;
-            l2 = l2.next;
-            place *= 10;
-        }
 
-        int sum = l1Value + l2Value;
-        System.out.println(String.format("%d+%d=%d",l1Value,l2Value,sum));
+        var carry = 0;
+        ListNode dummyHead = new ListNode(0);
+        ListNode currentNode = dummyHead;
 
-        return sumToListNode(sum);
+        while (l1 != null || l2 != null) {
+            var x = l1!=null?l1.val:0;
+            var y = l2!=null?l2.val:0;
+            var sum = x + y + carry;
+            var digit = sum % 10;
+            carry = sum / 10;
+            currentNode.next = new ListNode(digit);
+            currentNode = currentNode.next;
+            l1 = l1!=null?l1.next:null;
+            l2 = l2!=null?l2.next:null;
+        }
+        if (carry > 0) {
+
+            var node = new ListNode(carry);
+            currentNode.next = node;
+
+        }
+        return dummyHead.next;
     }
 
-    private ListNode sumToListNode(int sum) {
-        int place = (int) Math.pow(10d, (double) new String("" + sum).length() - 1);
-        int digit = sum /place;
-        var responseNode = new ListNode(digit);
-        var nextValue = sum - place*digit;
-        place /= 10;
-        while(place>=1){
-            digit = nextValue/place;
-            responseNode = new ListNode(digit,responseNode);
 
-            nextValue = nextValue - digit * place;
-            place /= 10;
+    public static final class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
         }
-        return responseNode;
-    }
 
-    public class ListNode {
-      int val;
-      ListNode next;
-      ListNode() {}
-      ListNode(int val) { this.val = val; }
-      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-  }
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+        public String toString() {
+            var rep = "" + val;
+            if (next != null) {
+                rep += "->" + next.toString();
+            }
+            return rep;
+        }
+    }
 
 }
