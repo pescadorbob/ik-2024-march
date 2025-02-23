93f662b53e976c2dc51a2cf770ecad5c91ada7af
Brent Fisher <bc.fisher@yahoo.com>
2025-02-23 14:48:28 -0700

    Reused a lot of code.

--- src/test/java/com/brent/expressions/evaluation/RelationshipEvaluatorFactory.java
+++ src/test/java/com/brent/expressions/evaluation/RelationshipEvaluatorFactory.java
@@ -1,32 +1,28 @@
-// RelationshipEvaluatorFactory.java
 package com.brent.expressions.evaluation;
 
 import com.brent.expressions.domain.ExpressionResult;
+import com.brent.expressions.domain.Operand;
 import com.brent.expressions.domain.RelationshipOperator;
 
 public class RelationshipEvaluatorFactory {
     public RelationshipEvaluator create(RelationshipOperator operator) {
         return switch (operator.getType()) {
-            case GREATER_THAN -> (lhs, rhs) -> {
-                @SuppressWarnings("unchecked")
-                Comparable<Object> leftValue = (Comparable<Object>) lhs.getValue();
-                return new ExpressionResult(leftValue.compareTo(rhs.getValue()) > 0);
-            };
-            case LESS_THAN -> (lhs, rhs) -> {
-                @SuppressWarnings("unchecked")
-                Comparable<Object> leftValue = (Comparable<Object>) lhs.getValue();
-                return new ExpressionResult(leftValue.compareTo(rhs.getValue()) < 0);
-            };
-            case LESS_THAN_OR_EQUAL -> (lhs, rhs) -> {
-                @SuppressWarnings("unchecked")
-                Comparable<Object> leftValue = (Comparable<Object>) lhs.getValue();
-                return new ExpressionResult(leftValue.compareTo(rhs.getValue()) <= 0);
-            };
-            case GREATER_THAN_OR_EQUAL -> (lhs, rhs) -> {
-                @SuppressWarnings("unchecked")
-                Comparable<Object> leftValue = (Comparable<Object>) lhs.getValue();
-                return new ExpressionResult(leftValue.compareTo(rhs.getValue()) >= 0);
-            };
+            case GREATER_THAN_OR_EQUAL -> (lhs, rhs) -> compareOperands(lhs, rhs, comp -> comp >= 0);
+            case GREATER_THAN -> (lhs, rhs) -> compareOperands(lhs, rhs, comp -> comp > 0);
+            case LESS_THAN -> (lhs, rhs) -> compareOperands(lhs, rhs, comp -> comp < 0);
+            case LESS_THAN_OR_EQUAL -> (lhs, rhs) -> compareOperands(lhs, rhs, comp -> comp <= 0);
         };
+    }
+
+    @SuppressWarnings("unchecked")
+    private ExpressionResult compareOperands(Operand<?> lhs, Operand<?> rhs, ComparisonPredicate predicate) {
+        Comparable<Object> leftValue = (Comparable<Object>) lhs.getValue();
+        int comparisonResult = leftValue.compareTo(rhs.getValue());
+        return new ExpressionResult(predicate.test(comparisonResult));
+    }
+
+    @FunctionalInterface
+    private interface ComparisonPredicate {
+        boolean test(int comparisonResult);
     }
 }
